7eedd9d9f045060acf4a4735d7f1b31b9f2db0d2
Laura Breiman <lauracle@mit.edu>
2014-11-15 16:06:24 -0500

    kinda dumb put made one way sortable

--- gameplay.js
+++ gameplay.js
@@ -1,7 +1,13 @@
 function connectSortable(){
+    $("#strand").css("height", $("#nucleotides").css("height"));
+
     $( "#nucleotides" ).sortable({
-              connectWith: ".connectedSortable", handle: ".nucleotide", revert: 250
+              connectWith: "#strand", handle: ".nucleotide", revert: 250
             }).disableSelection();
+    $( "#strand" ).sortable({
+         handle: ".nucleotide", revert: 250
+        }).disableSelection();
+    
 }
 
 $(document).ready(function() {
